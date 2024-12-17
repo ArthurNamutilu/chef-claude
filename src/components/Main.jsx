cbfd2e38de147c34139a29f26efbc4df5ff71040
ArthurNamutilu <arthurggnamutilu@gmail.com>
2024-12-17 23:49:43 +0300

    updating state with callback function

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,29 @@
 export default function Main() {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+  const ingredientsListItem = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Form Submitted");
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    ingredients.push(newIngredient);
+    console.log(ingredients);
+  }
+
   return (
     <main className="main">
-      <form className="add-ingredient-form">
+      <form className="add-ingredient-form" onSubmit={handleSubmit}>
         <input
           type="text"
-          name="query"
+          name="ingredient"
           placeholder="e.g oregano"
           aria-label="Add ingrediend"
         />
         <button type="submit">Add ingredient</button>
       </form>
+      <ul>{ingredientsListItem}</ul>
     </main>
   );
 }
